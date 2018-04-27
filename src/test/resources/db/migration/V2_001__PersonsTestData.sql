d6802880f3205f2f9eb3bfe3236eae23afeb19e3
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-04-27 12:19:39 +0545

    Create test dataset migration

---
+++ src/test/resources/db/migration/V2_001__PersonsTestData.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons(first_name, middle_name, last_name, address, email, gender, created_at)
+VALUES ('Sanish', '', 'Maharjan', 'Panga, Kirtipur', 'mjsanish@gmail.com', 'Male', CURRENT_TIMESTAMP),
+('Manish', '', 'Maharjan', 'Kalimati, Kathmandu', 'steps2light@gmail.com', 'Male', CURRENT_TIMESTAMP),
+('Sabina', '', 'Maharjan', 'Satungal, Kathmandu', 'sabimhz@gmail.com', 'Female', CURRENT_TIMESTAMP),
+('Rasna', '', 'Shakya', 'Khashi Bazzar, Kirtipur', 'rasna@gmail.com', 'Female', CURRENT_TIMESTAMP);
